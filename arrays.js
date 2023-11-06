aecab2564957064a90f434a65c7367b416045d09
Joel Luis Steffen <jo.lsteffen@gmail.com>
2023-11-05 21:15:41 -0300

    Ok

---
+++ arrays.js
@@ -0,0 +1,26 @@
+const deskArray = [
+  "pen",
+  "camera",
+  "phone",
+  "notebook",
+  "headphones",
+  "lightbulb",
+  "usb drive",
+];
+console.log("Original array:", deskArray);
+
+deskArray.pop();
+console.log("New array:", deskArray);
+
+deskArray.unshift(deskArray.pop());
+console.log("Last item is now first:", deskArray);
+
+deskArray.sort();
+console.log("Sorted array:", deskArray);
+
+const foundItem = deskArray.find((item) => item === "notebook");
+console.log("Found item:", foundItem);
+
+let remove = "notebook";
+deskArray.splice(deskArray.indexOf(remove), 1);
+console.log(`Array with "${remove}" removed:`, deskArray);
---
+++ estruturas-ondicionais.js
@@ -0,0 +1,13 @@
+console.log("true || 'abc' =", true || "abc");
+console.log("false || 'abc' =", false || "abc");
+console.log("=== ");
+console.log("true && 'efg' =", true && "efg");
+console.log("false && 'efg' =", false && "efg");
+console.log("=== ");
+console.log(" 1 || 'abc' ", 1 || "abc");
+console.log(" 0 || 'abc' ", 0 || "abc");
+console.log(" undefined || 'abc' ", undefined || "abc");
+console.log("=== ");
+console.log("0 || abc", 0 || "abc");
+console.log("null ?? abc", null ?? "abc");
+console.log("0 ?? abc", 0 ?? "abc");
